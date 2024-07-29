d55189dba8023275c86664c80e44f01afc8253c4
halvesromano <halvesromano@gmail.com>
2024-07-29 16:06:19 -0300

    Resolve exercicio 06 calculadora

--- web_dev_challenges/01_essencial/06_calculadora/calculator.py
+++ web_dev_challenges/01_essencial/06_calculadora/calculator.py
@@ -1,3 +1,5 @@
 def calculator():
   # TODO: Retorne o resultado da adição dos números informados pelo usuário
-  pass
+  numero1 = input('Insira o primeiro número: ')
+  numero2 = input('Insira o segundo número: ')
+  return numero1 + numero2
