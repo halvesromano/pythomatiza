b917afda2a749a808fed6a43d4dc2f6a031ad7bb
halvesromano <halvesromano@gmail.com>
2024-08-07 11:13:25 -0300

    Finaliza codigo do jogo de adivinhação

--- 006/jogo_advinhacao.py
+++ 006/jogo_advinhacao.py
@@ -1,7 +1,14 @@
-import random from random
+import random
 
 numero_secreto = random.randint(1, 100)
-palpite = input('Qual é o número inteiro secreto entre 1 e 100?')
 tentativa = 0
+palpite = int(input('Qual é o número inteiro secreto entre 1 e 100?'))
 
-while palpite != numero_secreto
+while (palpite != numero_secreto):
+    tentativa += 1
+
+    if palpite < numero_secreto:
+        palpite = int(input("O número secreto é MAIOR que o seu palpite! Digite novamente:"))
+    else:
+        palpite = int(input("O número secreto é MENOR que o seu palpite! Digite novamente:"))
+print("Parabéns, você acertou!")
