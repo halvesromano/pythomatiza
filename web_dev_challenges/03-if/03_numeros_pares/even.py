117a80807211fe5b715dbca68797332a66989990
halvesromano <halvesromano@gmail.com>
2024-10-15 17:24:33 -0300

    corrige alguns exercicios

--- web_dev_challenges/03-if/03_numeros_pares/even.py
+++ web_dev_challenges/03-if/03_numeros_pares/even.py
@@ -1,6 +1,9 @@
 def is_even(number):
     # Melhorar função is_even já implementada em 01-Essencial/07-aula/aula.py
-    # TODO: Retorne 'Número é par' se number for par e 'Número é inpar' se number for impar
+    # TODO: Retorne 'Número é par' se number for par e 'Número é impar' se number for impar
     # Exemplo 1: number = 2,  resultado = 'Número é par'
-    # Exemplo 2: number = 3,  resultado = 'Número é inpar'
-    pass
+    # Exemplo 2: number = 3,  resultado = 'Número é impar'
+    if number % 2 == 0:
+        return "Número é par"
+    else:
+        return "Número é impar"
